0682cfe05677c828371c45614d907c1fc9cd3a32
jasonm23 <jasonm23@gmail.com>
2015-06-01 22:44:03 +0800

    [Hotfix] add a bit of polish...

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Occur Follow
+
+A simple addition to occur key map to add instant highlighting of occurences in context
+
+A little demo is worth a thousand words...
+
+![](occur-follow.gif)
--- occur-follow.el
+++ occur-follow.el
@@ -1,3 +1,9 @@
+;;; occur-follow --- a simple addition to occur key map to add instant highlighting of occurences in context
+;;
+;;; Commentary:
+;;  A simple addition to occur-mode-map to add instant highlighting of occurences in context
+;;
+;;; Code:
 (define-key occur-mode-map (kbd "<up>")
   (lambda () (interactive)
     (occur-prev)
@@ -5,7 +11,7 @@
     (hl-line-mode 1)
     (recenter)
     (other-window 1)))
- 
+
 (define-key occur-mode-map (kbd "<down>")
   (lambda () (interactive)
     (occur-next)
@@ -13,5 +19,7 @@
     (hl-line-mode 1)
     (recenter)
     (other-window 1)))
-    
+
 (provide 'occur-follow)
+
+;;; occur-follow.el ends here
